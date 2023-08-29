1e7aea3b719476863e18790ef5ec799a1581e11f
Cassandra <cassandralelei013@gmail.com>
2023-08-29 06:25:11 -0700

    removed utils.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,18 @@
 #include "sort.h"
 
+/**
+* swap_ints - Swap two integers in an array.
+* @a: The first integer to swap.
+* @b: The second integer to swap.
+*/
+void swap_ints(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
 
 /**
  * bubble_sort - A function that sorts an array in ascending order.
