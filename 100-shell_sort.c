5aa91b8aa464369cb976616d628edbdcc0270944
Cassandra <cassandralelei013@gmail.com>
2023-08-29 06:44:27 -0700

    removed the utils file

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,5 +1,18 @@
 #include "sort.h"
 
+/**
+* swap_ints - A function that swap two integers in an array.
+* @a: The first integer to swap.
+* @b: The second integer to swap.
+*/
+void swap_ints(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
 
 /**
  * shell_sort - Sort an array of integers in ascending
